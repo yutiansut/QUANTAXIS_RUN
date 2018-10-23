71641554c03ca08a27ccaf7785cd5b2ba770fbbf
yutiansut <yutiansut@qq.com>
2018-10-23 12:58:26 +0800

    #udpate

--- quantaxis_run/query.py
+++ quantaxis_run/query.py
@@ -2,10 +2,10 @@ import pymongo
 
 
 def query_result():
-    return [item for item in pymongo.MongoClient().LQDESKTOP_JOB.JOB_LOG.find(
+    return [item for item in pymongo.MongoClient().quantaxis.JOB_LOG.find(
         {'status': {'$in': ['failed', 'success']}})]
 
 
 def query_onejob(filename):
-    return [item for item in pymongo.MongoClient().LQDESKTOP_JOB.JOB_LOG.find(
+    return [item for item in pymongo.MongoClient().quantaxis.JOB_LOG.find(
         {'filename': filename})]
