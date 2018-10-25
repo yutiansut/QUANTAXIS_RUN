ecd3459527539f93f376be8c1aa233619a9427bc
yutiansut <yutiansut@qq.com>
2018-10-25 12:26:53 +0800

    #

--- quantaxis_run/__init__.py
+++ quantaxis_run/__init__.py
@@ -7,7 +7,7 @@ import pymongo
 import datetime
 
 client_qa = pymongo.MongoClient().quantaxis.JOB_LOG
-
+client_joblist = pymongo.MongoClient().quantaxis.joblist
 client_qa.create_index('filename')
 app = Celery('tasks', backend='rpc://', broker='pyamqp://')
 
@@ -19,12 +19,14 @@ def quantaxis_run(self, shell_cmd, program='python'):
     shell_cmd = '{} "{}"'.format(program, shell_cmd)
 
     client_qa.insert({
-        'job_id':str(self.request.id),
+        'job_id': str(self.request.id),
         'source': program,
         'filename': filename,
         'time': str(datetime.datetime.now()),
         'message': 'start',
         'status': 'start'})
+    client_joblist.insert(
+        {'program': program, 'files': filename, 'status': 'running', 'job_id': str(self.request.id)})
     cmd = shlex.split(shell_cmd)
     p = subprocess.Popen(
         cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -33,21 +35,22 @@ def quantaxis_run(self, shell_cmd, program='python'):
         line = line.strip()
         if line:
             client_qa.insert({
-                'job_id':str(self.request.id),
+                'job_id': str(self.request.id),
                 'filename': filename,
                 'time': str(datetime.datetime.now()),
                 'message': str(line),
                 'status': 'running'})
     if p.returncode == 0:
         client_qa.insert({
-            'job_id':str(self.request.id),
+            'job_id': str(self.request.id),
             'filename': filename,
             'time': str(datetime.datetime.now()),
             'message': 'backtest run  success',
             'status': 'success'})
+        client_joblist.find_and_modify({'job_id': str(self.request.id)}, {'$set': {'status': 'success'}})
     else:
         client_qa.insert({
-            'job_id':str(self.request.id),
+            'job_id': str(self.request.id),
             'filename': filename,
             'time': str(datetime.datetime.now()),
             'message': str(p.returncode),
