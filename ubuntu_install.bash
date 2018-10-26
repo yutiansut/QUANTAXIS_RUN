34f566280929a28552567f405bfd71de6eac7a2f
yutiansut <yutiansut@qq.com>
2018-10-27 03:01:33 +0800

    #update

---
+++ ubuntu_install.bash
@@ -0,0 +1,11 @@
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
+
+sudo rabbitmqctl add_user admin admin 
+sudo rabbitmqctl set_user_tags admin administrator
