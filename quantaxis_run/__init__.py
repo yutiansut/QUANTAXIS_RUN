203bf3d398b26c4bafe7f266a572982fc164d7d6
yutiansut <yutiansut@qq.com>
2019-03-08 13:45:12 +0800

    #update just shell

--- quantaxis_run/__init__.py
+++ quantaxis_run/__init__.py
@@ -56,3 +56,49 @@ def quantaxis_run(self, shell_cmd, program='python'):
             'time': str(datetime.datetime.now()),
             'message': str(p.returncode),
             'status': 'failed'})
+
+
+@app.task(bind=True)
+def quantaxis_run_justshell(self, shell_cmd):
+
+    filename = shell_cmd
+    shell_cmd = '{}'.format(shell_cmd)
+
+    client_qa.insert({
+        'job_id': str(self.request.id),
+        'source': shell_cmd,
+        'filename': filename,
+        'time': str(datetime.datetime.now()),
+        'message': 'start',
+        'status': 'start'})
+    client_joblist.insert(
+        {'program': shell_cmd, 'files': filename, 'status': 'running', 'job_id': str(self.request.id)})
+    cmd = shlex.split(shell_cmd)
+    p = subprocess.Popen(
+        cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while p.poll() is None:
+        line = p.stdout.readline()
+        line = line.strip()
+        if line:
+            client_qa.insert({
+                'job_id': str(self.request.id),
+                'filename': filename,
+                'time': str(datetime.datetime.now()),
+                'message': str(line),
+                'status': 'running'})
+    if p.returncode == 0:
+        client_qa.insert({
+            'job_id': str(self.request.id),
+            'filename': filename,
+            'time': str(datetime.datetime.now()),
+            'message': 'backtest run  success',
+            'status': 'success'})
+        client_joblist.find_and_modify({'job_id': str(self.request.id)}, {
+                                       '$set': {'status': 'success'}})
+    else:
+        client_qa.insert({
+            'job_id': str(self.request.id),
+            'filename': filename,
+            'time': str(datetime.datetime.now()),
+            'message': str(p.returncode),
+            'status': 'failed'})
