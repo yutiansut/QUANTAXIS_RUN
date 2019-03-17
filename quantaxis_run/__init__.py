e2f83ef7aec45f8d5fd9ef9a9d63600f30328e1e
yutiansut <yutiansut@qq.com>
2019-03-17 17:09:50 +0800

    #update

--- quantaxis_run/__init__.py
+++ quantaxis_run/__init__.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 from celery import Celery, platforms
 from celery.schedules import crontab
@@ -18,9 +18,9 @@ client_qa.create_index('filename')
 
 
 class celeryconfig():
-    BROKER_URL = "pyamqp://"   # 使用redis存储任务队列
+    broker_url = "pyamqp://"   # 使用redis存储任务队列
     RESULT_BACKEND = "rpc://"  # 使用redis存储结果
-    CELERY_DEFAULT_QUEUE = 'default'
+    task_default_queue = 'default'
     task_serializer = 'json'
     result_serializer = 'json'
     accept_content = ['json', 'pickle']
@@ -28,17 +28,21 @@ class celeryconfig():
     enable_utc = True
     worker_hijack_root_logger = False  # celery默认开启自己的日志，可关闭自定义日志，不关闭自定义日志输出为空
     result_expires = 60 * 60 * 24  # 存储结果过期时间（默认1天）
-    CELERY_IMPORTS = (
+    imports = (
         "quantaxis_run"
     )
 
-    CELERYBEAT_SCHEDULE = {
-        'add':{
-            'task':'quantaxis_run.run_shell_daily',
-            'schedule':timedelta(seconds=10),
-            'args':(1,12)
-        }
+    beat_schedule = {
+        'daily': {
+            'task': 'quantaxis_run.monitor_daily',
+            'schedule': timedelta(days=1)
+        },
+        'trading': {
+            'task': 'quantaxis_run.monitor_trading',
+            'schedule': timedelta(seconds=10)
+        },
     }
+
 
 app = Celery('quantaxis_run', backend='rpc://', broker='pyamqp://')
 app.config_from_object(celeryconfig)
@@ -139,46 +143,10 @@ def run_shell(self, shell_cmd):
 
 
 @app.task(bind=True)
-def run_shell_daily(self, shell_cmd):
+def monitor_daily(self):
+    pass
 
-    filename = shell_cmd
-    shell_cmd = '{}'.format(shell_cmd)
 
-    client_qa.insert({
-        'job_id': str(self.request.id),
-        'source': shell_cmd,
-        'filename': filename,
-        'time': str(datetime.datetime.now()),
-        'message': 'start',
-        'status': 'start'})
-    client_joblist.insert(
-        {'program': shell_cmd, 'files': filename, 'status': 'running', 'type': 'qarun_daily' ,'job_id': str(self.request.id)})
-    cmd = shlex.split(shell_cmd)
-    p = subprocess.Popen(
-        cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while p.poll() is None:
-        line = p.stdout.readline()
-        line = line.strip()
-        if line:
-            client_qa.insert({
-                'job_id': str(self.request.id),
-                'filename': filename,
-                'time': str(datetime.datetime.now()),
-                'message': str(line),
-                'status': 'running'})
-    if p.returncode == 0:
-        client_qa.insert({
-            'job_id': str(self.request.id),
-            'filename': filename,
-            'time': str(datetime.datetime.now()),
-            'message': 'backtest run  success',
-            'status': 'success'})
-        client_joblist.find_and_modify({'job_id': str(self.request.id)}, {
-                                       '$set': {'status': 'success'}})
-    else:
-        client_qa.insert({
-            'job_id': str(self.request.id),
-            'filename': filename,
-            'time': str(datetime.datetime.now()),
-            'message': str(p.returncode),
-            'status': 'failed'})
+@app.task(bind=True)
+def monitor_trading(self):
+    pass
