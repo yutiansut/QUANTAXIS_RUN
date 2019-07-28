39813253c5bfea58c6ac07397b30ccc80addd3b5
Vincent yu <yutiansut@qq.com>
2019-07-28 10:50:48 +0800

    Update ubuntu_install.bash

--- ubuntu_install.bash
+++ ubuntu_install.bash
@@ -1,11 +1,9 @@
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list
 
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
 
-sudo apt-get update -y
-
-sudo apt-get install rabbitmq-server -y
-sudo rabbitmq-plugins enable rabbitmq_management
-
-sudo rabbitmqctl add_user admin admin 
-sudo rabbitmqctl set_user_tags admin administrator
+apt-get update -y --force-yes && apt-get install rabbitmq-server -y \
+&& rabbitmq-plugins enable rabbitmq_management \
+&& rabbitmqctl add_user admin admin \
+&& rabbitmqctl set_user_tags admin administrator \
+&& rabbitmqctl  set_permissions -p "/" admin '.*' '.*' '.*' \
