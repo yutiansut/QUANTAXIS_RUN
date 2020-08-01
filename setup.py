7464b0745fdabd2bd04feb25794dc539c5095d87
Vincent yu <yutian@quantaxis.com.cn>
2020-08-01 11:45:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
     ],
-    install_requires=['celery>4', 'quantaxis>=1.3.1'],
+    install_requires=['celery>4', 'quantaxis>=1.3.1', 'qaenv'],
     keywords=KEYWORDS,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
